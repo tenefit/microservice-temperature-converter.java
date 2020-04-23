7839473aff443151e89043318d7d26d3151e0de8
Robin Zimmermann <robin.zimmermann@kaazing.com>
2020-04-23 14:16:16 -0700

    Handle non-JSON payload from sensors

--- src/test/java/com/tenefit/demo/microservice/temperatureConverter/SensorsMessageHandlerTest.java
+++ src/test/java/com/tenefit/demo/microservice/temperatureConverter/SensorsMessageHandlerTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -46,6 +47,68 @@ public class SensorsMessageHandlerTest
         when(producer.send(any(ProducerRecord.class))).thenReturn(future);
 
         SensorsMessageHandler handler = new SensorsMessageHandler(producer, "readings");
+        handler.handleMessage(record, TemperatureUnit.F);
+
+        ArgumentCaptor<ProducerRecord<String, String>> sendArg = ArgumentCaptor.forClass(ProducerRecord.class);
+        verify(producer).send(sendArg.capture());
+
+        assertEquals("4", sendArg.getValue().key());
+
+        assertEquals("readings", sendArg.getValue().topic());
+
+        assertEquals(1, sendArg.getValue().headers().toArray().length);
+
+        Header row = sendArg.getValue().headers().lastHeader("row");
+        assertNotNull("missing row header", row);
+        assertArrayEquals("9".getBytes(UTF_8), row.value());
+
+        try (JsonReader outputJson = Json.createReader(new StringReader(sendArg.getValue().value())))
+        {
+            JsonObject output = outputJson.readObject();
+            assertEquals(3, output.size());
+
+            String id = output.getString("id");
+            assertNotNull(id);
+            assertEquals("4", id);
+
+            String unit = output.getString("unit");
+            assertNotNull(unit);
+            assertEquals("F", unit);
+
+            int value = output.getInt("value", Integer.MAX_VALUE);
+            assertEquals(32, value);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldSkipMessageWithNonJsonPayload() throws Exception
+    {
+        final KafkaProducer<String, String> producer = mock(KafkaProducer.class);
+        final ConsumerRecord<String, String> record = mock(ConsumerRecord.class);
+        final Future<RecordMetadata> future = mock(Future.class);
+
+        RecordHeaders recordHeaders = new RecordHeaders();
+        recordHeaders.add("row", "9".getBytes(UTF_8));
+        when(record.headers()).thenReturn(recordHeaders);
+        when(record.key()).thenReturn("4");
+        when(record.value()).thenReturn("xyz-not-JSON");
+        when(producer.send(any(ProducerRecord.class))).thenReturn(future);
+
+        SensorsMessageHandler handler = new SensorsMessageHandler(producer, "readings");
+        handler.handleMessage(record, TemperatureUnit.F);
+
+        verify(producer, never()).send(any());
+
+        // Ensure that a subsequent valid message is processed correctly
+        recordHeaders = new RecordHeaders();
+        recordHeaders.add("row", "9".getBytes(UTF_8));
+        when(record.headers()).thenReturn(recordHeaders);
+        when(record.key()).thenReturn("4");
+        when(record.value()).thenReturn("{\"id\":\"4\",\"unit\":\"C\",\"value\":0}");
+        when(producer.send(any(ProducerRecord.class))).thenReturn(future);
+
+        handler = new SensorsMessageHandler(producer, "readings");
         handler.handleMessage(record, TemperatureUnit.F);
 
         ArgumentCaptor<ProducerRecord<String, String>> sendArg = ArgumentCaptor.forClass(ProducerRecord.class);
